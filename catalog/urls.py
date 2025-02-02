1ceee695bfeb4c1dbf4e84bf92179b5a5459a1de
Nagaeva Elina <05batman@mail.ru>
2025-02-03 03:48:14 +0400

    Добавление форм

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, HomeView, ContactsView
+from catalog.views import ProductListView, ProductDetailView, HomeView, ContactsView, ProductUpdateView, ProductDeleteView, ProductCreateView
 app_name = CatalogConfig.name
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('', ProductListView.as_view(), name='product_list'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path("product/create", ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete", ProductDeleteView.as_view(), name="product_delete")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from catalog.models import Product
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+from .forms import ProductForm
+from django.urls import reverse_lazy
 
 
 
@@ -20,3 +22,18 @@ class ContactsView(TemplateView):
 
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:product_list")
